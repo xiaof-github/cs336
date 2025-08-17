99c243f5c1e4cead1c0c03aab5fe611a373fef53
xiaof <xffit@163.com>
2025-08-17 19:51:40 +0800

    [A]bpe train copy

--- tests/adapters.py
+++ tests/adapters.py
@@ -8,7 +8,7 @@ from jaxtyping import Float, Int
 import numpy.typing as npt
 import torch
 from torch import Tensor
-from cs336_basics.my_train_bpe import *
+from cs336_basics.bpe import *
 
 def run_linear(
     d_in: int,
